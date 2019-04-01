f3525caac6a08db4d76bbad38b7e73c88c964dba
james <james.bridgeford@cmhworks.com>
2019-03-31 19:25:49 -0500

    Should really look at what I'm committing first.  Didn't add the changes to Enrollment at first.

--- src/main/java/com/company/Enrollment.java
+++ src/main/java/com/company/Enrollment.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.Comparator;
 
 public class Enrollment implements Comparable<Enrollment>{
     private String userId;
@@ -58,7 +59,23 @@ public class Enrollment implements Comparable<Enrollment>{
 
         if(this.userId.equals(o.userId) && this.firstName.equals(o.firstName) && this.lastName.equals(o.lastName)
                 && this.insuranceCompany.equals(o.insuranceCompany) && this.version < o.version) {return -1;}
-
-        return 0;
+        if(this.userId.equals(o.userId) && this.firstName.equals(o.firstName) && this.lastName.equals(o.lastName)
+                && this.insuranceCompany.equals(o.insuranceCompany) && this.version == o.version) {return 0;}
+        return 99;
     }
+
+    public static Comparator<Enrollment> SortByName = (s1, s2) -> {
+        String e1LastName = s1.getLastName().toUpperCase();
+        String e2LastName = s2.getLastName().toUpperCase();
+        String e1FirstName = s1.getFirstName().toUpperCase();
+        String e2FirstName = s2.getFirstName().toUpperCase();
+
+        //ascending order
+        int lastNameCompare =  e1LastName.compareTo(e2LastName);
+
+        //Since we're sorting by last name first if they are not equal then we return.  Otherwise we go to firstname
+        if(lastNameCompare != 0) return lastNameCompare;
+
+        return e1FirstName.compareTo(e2FirstName);
+    };
 }
